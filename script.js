ac7ae84e429be466a4c20606cec2b76e0ecdd269
ChinGuang <ganchinguang0622@gmail.com>
2025-10-02 11:30:25 +0800

    Add human choice function
    Introduces the `getHumanChoice` function to prompt the user for their move and validate it against the allowed options.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+const RockPaperScissors = {
+    rock: "rock",
+    scissors: "scissors",
+    paper: "paper"
+}
+
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     switch (randomChoice) {
         case 0:
-            return "rock";
+            return RockPaperScissors.rock;
         case 1:
-            return "scissors";
+            return RockPaperScissors.scissors;
         case 2:
-            return "paper";
+            return RockPaperScissors.paper;
         default:
             return getComputerChoice();
     }
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Rock, scissors, paper!")?.toLowerCase();
+    if (Object.values(RockPaperScissors).includes(humanChoice)) return humanChoice;
 }
