307d06024d6d98badc17be9ee7a66839b8ffe74e
ChinGuang <ganchinguang0622@gmail.com>
2025-10-02 11:25:22 +0800

    Add computer choice logic
    Implements `getComputerChoice` function to randomly select rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomChoice = Math.floor(Math.random() * 3);
+    switch (randomChoice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "scissors";
+        case 2:
+            return "paper";
+        default:
+            return getComputerChoice();
+    }
+}
