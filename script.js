6e93fa0a4814b01c7b34f9653c3111675bffba7e
ChinGuang <ganchinguang0622@gmail.com>
2025-10-02 12:01:31 +0800

    Add game logic and rules
    Introduces constants for game choices and results, a winning rule object, and a `playRound` function to determine the outcome of a single round. Also adds a `toCapitalize` helper function and calls `playRound` at the end.

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ const RockPaperScissors = {
     paper: "paper"
 }
 
+const RockPaperScissorsResultType = {
+    WIN: "win",
+    TIE: "tie",
+    LOSE: "lose",
+}
+
+const RockPaperScissorsWinningRule = {
+    [RockPaperScissors.rock]: {
+        [RockPaperScissors.rock]: RockPaperScissorsResultType.TIE,
+        [RockPaperScissors.scissors]: RockPaperScissorsResultType.WIN,
+        [RockPaperScissors.paper]: RockPaperScissorsResultType.LOSE,
+    },
+    [RockPaperScissors.scissors]: {
+        [RockPaperScissors.rock]: RockPaperScissorsResultType.LOSE,
+        [RockPaperScissors.scissors]: RockPaperScissorsResultType.TIE,
+        [RockPaperScissors.paper]: RockPaperScissorsResultType.WIN,
+    },
+    [RockPaperScissors.paper]: {
+        [RockPaperScissors.rock]: RockPaperScissorsResultType.WIN,
+        [RockPaperScissors.scissors]: RockPaperScissorsResultType.LOSE,
+        [RockPaperScissors.paper]: RockPaperScissorsResultType.TIE,
+    }
+}
+
+
+
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     switch (randomChoice) {
@@ -25,3 +51,28 @@ function getHumanChoice() {
     const humanChoice = prompt("Rock, scissors, paper!")?.toLowerCase();
     if (Object.values(RockPaperScissors).includes(humanChoice)) return humanChoice;
 }
+
+function toCapitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    const result = RockPaperScissorsWinningRule[humanChoice]?.[computerChoice];
+    switch (result) {
+        case RockPaperScissorsResultType.WIN:
+            console.log(`You win! ${toCapitalize(humanChoice)} beats ${toCapitalize(computerChoice)}`);
+            humanScore++;
+            break;
+        case RockPaperScissorsResultType.TIE:
+            console.log(`It's a tie`);
+            break;
+        case RockPaperScissorsResultType.LOSE:
+            console.log(`You lose! ${toCapitalize(computerChoice)} beats ${toCapitalize(humanChoice)}`);
+            computerScore++;
+            break;
+        default:
+            throw new Error(`Unexpected value: ${result}`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
